4a51fdbe428d843c4c67e7a72401d7804d750840
Eason.L <easonleung@qq.com>
2022-03-12 15:42:36 +0800

    Update TotaraEnvironmentInit.sh

--- TotaraEnvironmentInit.sh
+++ TotaraEnvironmentInit.sh
@@ -31,17 +31,17 @@ Latest commit 6765407 on Mar 9, 2020
 
 echo ""
 echo ""
-echo "PowerMask System Initialization"
+echo "PowerMask-Totara System Initialization"
 echo "=========================================="
 echo "WARNING:"
 echo "This Machine is Huge Psyho System. It base"
-echo "on Debian9.x. If you want to install it at"
+echo "on Debian11.x. If you want to install it at"
 echo "Different System. Please Fuck Off!!!"
 echo ""
 echo "+------------------------------------+"
 echo "| AUTHOR:    Eason.L                 |"
-echo "| DATETIME:  2020.03.08              |"
-echo "| SYSTEM:    Debian9.x               |"
+echo "| DATETIME:  2022.03.12              |"
+echo "| SYSTEM:    Debian11.x               |"
 echo "| TOKEN:     NONE                    |"
 echo "+------------------------------------+"
 echo ""
@@ -154,66 +154,41 @@ echo "========================================="
 echo ""
 ################################################
 
-# ==== INSTALL PHP7.3 ======================== #
+# ==== INSTALL PHP7.4 ======================== #
 echo ""
-echo "===> Install PHP7.3 ..."
+echo "===> Install PHP7.4 ..."
 echo ""
 echo ""
 echo "... install base tools ..."
 echo ""
 echo "$rootpassword" | sudo -S apt-get -y install apt-transport-https ca-certificates
 
-echo ""
-echo "... add sury source ..."
-echo ""
-echo "$rootpassword" | sudo -S wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
+#echo ""
+#echo "... add sury source ..."
+#echo ""
+#echo "$rootpassword" | sudo -S wget -O /etc/apt/trusted.gpg.d/php.gpg https://packages.sury.org/php/apt.gpg
 
 echo ""
 echo "... update source ..."
 echo ""
-echo "$rootpassword" | sudo -S sh -c 'echo "deb https://packages.sury.org/php/ stretch main" > /etc/apt/sources.list.d/php.list'
+echo "$rootpassword" | sudo -S sh -c 'echo "deb https://packages.sury.org/php/ bullseye main" > /etc/apt/sources.list.d/php.list'
 echo "$rootpassword" | sudo -S apt-get update
 
 echo ""
-echo "... Install PHP7.3 ..."
+echo "... Install PHP7.4 ..."
 echo ""
-echo "$rootpassword" | sudo -S apt-get install php7.3 -y
+echo "$rootpassword" | sudo -S apt-get install php7.4 -y
 
 # ==== INSTALL MARIADB ======================== #
 echo ""
-echo "===> Install MariaDB ..."
+echo "===> Install MariaDB Server..."
 echo ""
-echo "$rootpassword" | sudo -S apt-get install -y mysql-server mysql-client
-
-
-# ==== INSTALL MongoDB ======================== #
+echo "$rootpassword" | sudo -S apt-get install -y mariadb-server
 echo ""
-echo "===> Install MongoDB ..."
+echo "... Start&Set MariaDB Server Startup ..."
 echo ""
-echo "... get apt-key from www.mongodb.org ..."
-echo ""
-curl https://www.mongodb.org/static/pgp/server-4.0.asc | sudo apt-key add -
-
-echo ""
-echo "... add source ..."
-echo ""
-echo "$rootpassword" | sudo -S sh -c 'echo "deb http://repo.mongodb.org/apt/debian stretch/mongodb-org/4.0 main" > /etc/apt/sources.list.d/php.list'
-
-echo ""
-echo "... update source ..."
-echo ""
-echo "$rootpassword" | sudo -S apt-get update
-
-echo ""
-echo "... install mongodb ..."
-echo ""
-echo "$rootpassword" | sudo -S apt-get install mongodb-org -y
-
-echo ""
-echo "... start mongodb service ..."
-echo ""
-echo "$rootpassword" | sudo -S systemctl enable mongod
-echo "$rootpassword" | sudo -S systemctl start mongod
+echo "$rootpassword" | sudo systemctl start mariadb
+echo "$rootpassword" | sudo systemctl enable mariadb
 
 
 # ==== INSTALL NGINX ======================== #
@@ -223,87 +198,25 @@ echo ""
 echo "$rootpassword" | sudo -S apt-get install nginx -y
 
 
-
-
-# ==== INSTALL NVM ======================== #
-echo ""
-echo "===> Install nvm ..."
-echo ""
-
-# curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.35.3/install.sh | bash
-
-export NVM_DIR="$HOME/.nvm"
-[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"					 # This loads nvm
-[ -s "$NVM_DIR/bash_completion" ] && \. "$NVM_DIR/bash_completion"	 # This loads nvm bash_completion
-
-echo ""
-echo "===> Install node.js 12.x ..."
-echo ""
-## nvm install --lts		# Too Slow
-## nvm install 8.16.0		# Too Slow
-curl -sL https://deb.nodesource.com/setup_12.x | sudo bash -
-echo "$rootpassword" | sudo -S apt-get install nodejs 
-
-##echo ""
-##echo "===> Install node.js 8.x ..."
-##echo ""
-##curl -sL https://deb.nodesource.com/setup_8.x | sudo bash -
-##echo "$rootpassword" | sudo -S apt-get install nodejs
-
-
 ################################################
 echo ""
 echo ""
-echo "INSTALL ADAPT PLATFORM"
+echo "INSTALL TOTARA PLATFORM"
 echo "========================================="
 echo ""
 ################################################
 
 
-# ==== INSTALL ADAPT AUTHORING ======================== #
+# ==== INSTALL TOTARA ======================== #
 echo ""
 echo "===> Install Adapt_authoring ..."
 echo ""
 echo "... clone Adapt_authoring ..."
 echo ""
-git clone https://github.com/adaptlearning/adapt_authoring.git
-cd adapt_authoring
-npm install --production && node install
 
 
 
-# ==== INSTALL ADAPT FRAMEWORK ======================== #
-echo ""
-echo "===> Install Adapt_framework ..."
-echo ""
-echo ""
-echo "... clone Adapt_authoring ..."
-echo ""
-git clone https://github.com/adaptlearning/adapt_framework.git
 
-echo "... install grunt ..."
-echo ""
-echo "$rootpassword" | sudo -S apt-get install grunt 
-
-echo ""
-echo "... install cnpm ..."
-echo ""
-echo "$rootpassword" | sudo -S npm install -g cnpm --registry=https://registry.npm.taobao.org 
-
-echo ""
-echo "... install adapt_framework ..."
-echo ""
-cd adapt_framework-master
-cnpm install
-adapt install
-
-echo ""
-echo "... install rub ..."
-echo ""
-echo "$rootpassword" | sudo -S chown -R $USER /usr/lib
-echo "$rootpassword" | sudo -S npm install -g grunt-cli
-echo "$rootpassword" | sudo -S npm install -g rub-cli
-pkexec chown root /usr/lib/sudo/sudoers.so
 
 echo ""
 echo ""
