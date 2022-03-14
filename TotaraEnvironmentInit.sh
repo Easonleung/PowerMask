08d65b01de4cfd637969bb074f71f7b29470b1dd
Eason.L <easonleung@qq.com>
2022-03-14 21:08:48 +0800

    Update TotaraEnvironmentInit.sh

--- TotaraEnvironmentInit.sh
+++ TotaraEnvironmentInit.sh
@@ -180,7 +180,7 @@ echo "... Start&Set MariaDB Server Startup ..."
 echo ""
 echo "$rootpassword" | sudo systemctl start mariadb
 echo "$rootpassword" | sudo systemctl enable mariadb
-echo "$rootpassword" | sudo mysql_secure_installation
+# echo "$rootpassword" | sudo mysql_secure_installation
 
 
 # ==== INSTALL NGINX ======================== #
